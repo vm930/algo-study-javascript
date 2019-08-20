e236622eeb76932c9be2bc45d240f10de95494ed
Victoria Mei <vm930@hotmail.com>
2019-08-20 15:31:53 -0400

    adding more methods in dequeue and enqueue

--- queue.js
+++ queue.js
@@ -1,10 +1,36 @@
 //Create a Queue class
+//when implementing enque and dequeue, we should be add from the tail, and remove from the head to maximizing efficiency.
 
 class Queue {
 	constructor() {
 		this.first = null;
 		this.last = null;
 		this.size = 0;
+	}
+
+	enqueue(val) {
+		var newNode = new Node(val); //create a Node instance
+		if (!this.first) {
+			//edge cases
+			this.first = newNode;
+			this.last = newNode;
+		} else {
+			this.last.next = newNode;
+			this.last = newNode;
+		}
+		return ++this.size;
+	}
+
+	dequeue() {
+		if (!this.first) return null;
+		var temp = this.first;
+		if (this.first === this.last) {
+			this.last = null;
+			//edge cases when only 1 thing in the list
+		}
+		this.first = this.first.next;
+		this.size--;
+		return temp.value;
 	}
 }
 
