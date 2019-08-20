aa9f036059df4321de54ae67b994fe42a3b05807
Victoria Mei <vm930@hotmail.com>
2019-08-20 15:19:38 -0400

    create a file

---
+++ queue.js
@@ -0,0 +1,16 @@
+//Create a Queue class
+
+class Queue {
+	constructor() {
+		this.first = null;
+		this.last = null;
+		this.size = 0;
+	}
+}
+
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
+}
