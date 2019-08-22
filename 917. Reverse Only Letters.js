ff0809c02e52c5339c4261d487df0d1b3f83ee76
Victoria Mei <vm930@hotmail.com>
2019-08-22 12:00:20 -0400

    solve question 917

--- 917. Reverse Only Letters.js
+++ 917. Reverse Only Letters.js
@@ -17,26 +17,55 @@ Input: "Test1ng-Leet=code-Q!"
 Output: "Qedo1ct-eeLg=ntse-T!"
 */
 
-var reverseOnlyLetters = function (S) {
-    //split string into array
-    let arr = S.split("")
-    //using two pointers , s & e  
+var reverseOnlyLetters = function(S) {
+	//split string into array
+	let arr = S.split('');
+	//using two pointers , s & e
 
-    function swap (arr){
-        let s = 0;
-        let e = arr.length - 1;
-        while (arr[s]!== e) {
-            if (arr[s].charCodeAt() >= 65 && arr[s].charCodeAt() <= 90 || arr[s].charCodeAt() >= 97 && arr[s].charCodeAt() <= 122){
-                if (arr[e].charCodeAt() >= 65 && arr[e].charCodeAt() <= 90 || arr[e].charCodeAt() >= 97 && arr[e].charCodeAt() <= 122)
-                let temp = arr[e];
-                arr[e] = arr[s];
-                arr[s] = temp;
-            }else{
-                e--;
-            }
-        }
-        charCodeAt()
-    }    
-    //write a swap function to pass in an array 
-    //if the charcodeat 65~90 upper 97~122 lower S[i].charcodeat()
+	// Example 3:
+	// Input: "Test1ng-Leet=code-Q!"
+	// Output: "Qedo1ct-eeLg=ntse-T!"
+
+	//function check if character is a letter?
+	function isLetter(char) {
+		let lowerChar = char.toLowercase();
+		//if the charcodeat 65~90 upper 97~122 lower S[i].charcodeat()
+		if (lowerChar.charCodeAt() >= 97 && lowerChar.charCodeAt() <= 122) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function swap(arr) {
+		let s = 0;
+		let e = arr.length - 1;
+
+		while (s < e) {
+			if (isLetter(arr[s]) && isLetter(arr[e])) {
+				let temp = arr[e];
+				arr[e] = arr[s];
+				arr[s] = temp;
+				s++;
+				e--;
+			} else {
+				if (!isLetter(arr[s])) {
+					s++;
+				}
+				if (!isLetter(arr[e])) {
+					e--;
+				}
+				// if (!isLetter(arr[e]) && !isLetter(arr[s])) {
+				// 	s++;
+				// 	e--;
+				// }
+			}
+		}
+		return arr;
+	}
+
+	let swaparr = swap(arr);
+	return swaparr.join('');
+
+	//write a swap function to pass in an array
 };
