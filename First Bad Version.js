635fcd49237c261f3a8461173667ac1ce7923406
Victoria Mei <vm930@hotmail.com>
2019-08-23 21:07:18 -0400

    new file from leetcode

---
+++ First Bad Version.js
@@ -0,0 +1,20 @@
+//First Bad Version
+/*
+Given n = 5, and version = 4 is the first bad version.
+
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+
+Then 4 is the first bad version.
+*/
+
+var solution = function(isBadVersion) {
+	/**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+	return function(n) {};
+};
+
+solution(isBadVersion);
