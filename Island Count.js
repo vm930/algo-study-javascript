ee1f0f0f6af2d971c62a3cd967dde31c2ed47408
Victoria Mei <vm930@hotmail.com>
2019-08-29 16:47:51 -0400

    finished island code

--- Island Count.js
+++ Island Count.js
@@ -30,13 +30,10 @@ function getNumberOfIslands(grid) {
 	//false
 	//then you also need to update top left right bottom ele in the copy to be trues
 	//increament count +1;
-
 	//check current current ele [i][j] left [i][j-1] right [i][j+1] top [i-1][j]  below [i+1][j]
-
 	//return the counter
 
 	let copy = [];
-
 	//initialize a copy of an array with all falses
 	for (let i = 0; i < grid.length; i++) {
 		let copy2 = [];
@@ -48,29 +45,65 @@ function getNumberOfIslands(grid) {
 	// console.log(copy);
 	let count = 0;
 
-	for (let i = 0; i < grid.length; i++) {
-		for (let j = 0; j < grid[i].length; j++) {
-			if (grid[j][i] === 1) {
-				if (copy[j][i] === false) {
-					if (copy[j - 1][i] !== undefined) {
-						copy[j - 1][i] = true;
-					}
-					if (copy[j + 1][i] !== undefined) {
-						copy[j + 1][i] = true;
-					}
-					if (copy[j][i + 1] !== undefined) {
-						copy[j][i + 1] = true;
-					}
-					if (copy[j][i - 1] !== undefined) {
-						copy[j][i - 1] = true;
-					}
-					count++;
-				}
-				//if true, move on
-			} //else if current ele === 0 move on
+	function validation(i, j) {
+		let valid = [];
+		// let left = [i, j-1]
+		// let right = [i, j+1]
+		// let top = [i-1, j]
+		// let bottom = [i+1,j]
+		if (i - 1 >= 0) {
+			valid.push([ i - 1, j ]); //top
+		}
+		if (j - 1 >= 0) {
+			valid.push([ i, j - 1 ]); //left
+		}
+		if (j + 1 < grid[0].length) {
+			valid.push([ i, j + 1 ]); //right
+		}
+		if (i + 1 < grid.length) {
+			valid.push([ i + 1, j ]); //bottom
+		}
+		// console.log(valid)
+		return valid;
+	}
+
+	function findIsland(i, j) {
+		copy[i][j] = true;
+		let validarr = validation(i, j);
+		//check to see every coordinates in validarr and see if its a 1,
+		// if so call function again, else move on
+		for (let k = 0; k < validarr.length; k++) {
+			let indexI = validarr[k][0];
+			let indexJ = validarr[k][1];
+
+			if (grid[indexI][indexJ] === 1 && copy[indexI][indexJ] === false) {
+				findIsland(indexI, indexJ);
+			}
 		}
 	}
+
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			if (grid[i][j] === 1 && copy[i][j] === false) {
+				count++;
+				findIsland(i, j);
+			}
+			// console.log(count);
+			//if true, move on
+		} //else if current ele === 0 move on
+	}
+	console.log(copy);
 	return count;
 }
 
-getNumberOfIslands([ [ 0, 1, 0, 1, 0 ], [ 0, 0, 1, 1, 1 ], [ 1, 0, 0, 1, 0 ], [ 0, 1, 1, 0, 0 ], [ 1, 0, 1, 0, 1 ] ]);
+console.log(
+	getNumberOfIslands([
+		[ 0, 1, 0, 1, 0 ],
+		[ 0, 0, 1, 1, 1 ],
+		[ 1, 0, 0, 1, 0 ],
+		[ 0, 1, 1, 0, 0 ],
+		[ 1, 0, 1, 0, 1 ]
+	])
+);
+
+// console.log(getNumberOfIslands([ [ 0, 1, 0 ], [ 0, 1, 0 ], [ 1, 1, 0 ] ]));
