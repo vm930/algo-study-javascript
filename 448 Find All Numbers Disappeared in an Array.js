ce8ca89a695d8addda71be280fa6170017fdd579
Victoria Mei <vm930@hotmail.com>
2019-09-10 21:25:34 -0400

    donezo

--- 448 Find All Numbers Disappeared in an Array.js
+++ 448 Find All Numbers Disappeared in an Array.js
@@ -14,4 +14,40 @@ Output:
 [5,6]
 */
 
-var findDisappearedNumbers = function(nums) {};
+var findDisappearedNumbers = function(nums) {
+	// iterate nums to create a hash map of key num value "y";
+	// cearte a solution array to return at the end
+	// create new array for counts newarray = []
+	// iterate to create newArray
+	// for(let i = 0; i<nums.length; i ++){
+	//   newarray.push(i+1);
+	//}
+	//loop through newarray
+	//if lookup[newarr[i]] === "y" ignore
+	//if lookup[newarr[i]] === undefined solution.push[newarr[i]]
+	//return solution
+
+	let lookup = {};
+	let solution = [];
+	let newArr = [];
+
+	for (let i = 0; i < nums.length; i++) {
+		if (lookup[nums[i]] === undefined) {
+			lookup[nums[i]] = 'y';
+		}
+	}
+
+	for (let j = 0; j < nums.length; j++) {
+		newArr.push(j + 1);
+	}
+
+	for (let k = 0; k < newArr.length; k++) {
+		if (lookup[newArr[k]] === undefined) {
+			solution.push(newArr[k]);
+		}
+	}
+
+	return solution;
+};
+
+console.log(findDisappearedNumbers([ 4, 3, 2, 7, 8, 2, 3, 1 ]));
