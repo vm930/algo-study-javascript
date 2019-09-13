9a21f6983dc733e44d8f8ae24facc21970d96a98
Victoria Mei <vm930@hotmail.com>
2019-09-13 13:18:37 -0400

    2 solutions

--- 70. Climbing Stairs.js
+++ 70. Climbing Stairs.js
@@ -21,3 +21,41 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 */
+
+var climbStairs = function(n) {
+	//using recursion
+	//base cases are 0, 1, 2
+	if (n === 0) {
+		return 0;
+	}
+	if (n === 1) {
+		return 1;
+	}
+	if (n === 2) {
+		return 2;
+	}
+
+	let solution;
+	solution = climbStairs(n - 1) + climbStairs(n - 2);
+	return solution;
+};
+
+//another solution
+
+var climbStairs = function(n) {
+	if (n < 2) return 1; //edge case
+	let count = 0;
+	let result = [ 1, 1 ];
+
+	for (let i = 2; i < n + 1; i++) {
+		result[i] = result[i - 2] + result[i - 1];
+	}
+	count = result[result.length - 1];
+
+	return count;
+};
+
+console.log(climbStairs(2));
+console.log(climbStairs(1));
+console.log(climbStairs(5));
+console.log(climbStairs(6));
