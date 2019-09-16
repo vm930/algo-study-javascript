416f2149d20b9be875597ccd46a009175b2e0e3d
Victoria Mei <vm930@hotmail.com>
2019-09-15 22:38:25 -0400

    solve island

--- 463. Island Perimeter.js
+++ 463. Island Perimeter.js
@@ -18,3 +18,50 @@ Output: 16
 
 Explanation: The perimeter is the 16 yellow stripes in the image below:
 */
+var islandPerimeter = function(grid) {
+	/* use two for loops 
+  use perimeter = 0
+  check if grid[i][j] === 0 move on 
+  if grid[i][j] === 1 {
+      1.check to see top, bottom left and right 
+            (if they are === 0 || if they are === undefined ) then increament perimeter
+             else move on 
+  }
+  at the end return perimeter 
+  */
+	if (grid === null) {
+		return 0;
+	}
+	let perimeter = 0;
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			if (grid[i][j] === 1) {
+				perimeter += perimeterCount(i, j);
+			}
+		}
+	}
+
+	function perimeterCount(i, j) {
+		//top [i-1, j]
+		//bottom [i+1, j]
+		//left [i, j-1]
+		//right [i, j+1]
+		let count = 0;
+		if (i - 1 < 0 || grid[i - 1][j] === 0) {
+			count++;
+		} //top
+		if (i + 1 >= grid.length || grid[i + 1][j] === 0) {
+			count++;
+		} //bottom
+		if (j - 1 < 0 || grid[i][j - 1] === 0) {
+			count++;
+		} //left
+		if (j + 1 >= grid[i].length || grid[i][j + 1] === 0) {
+			count++;
+		} //right
+		return count;
+	}
+	return perimeter;
+};
+
+console.log(islandPerimeter([ [ 0, 1, 0, 0 ], [ 1, 1, 1, 0 ], [ 0, 1, 0, 0 ], [ 1, 1, 0, 0 ] ]));
