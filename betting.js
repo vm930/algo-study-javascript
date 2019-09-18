9b884a083567334802726d13a0cc368d266cc397
Victoria Mei <vm930@hotmail.com>
2019-09-17 22:14:15 -0400

    solved

--- betting.js
+++ betting.js
@@ -1,5 +1,9 @@
 /*
+N = Number of chips in a bet
+K = number of times you can go all in 
 
+return the possible smallest number of rounds 
+betting on 2 chips is not considered as all in 
 */
 
 function solution(N, K) {
@@ -7,32 +11,23 @@ function solution(N, K) {
 	let solution = 0;
 	// create a variable to store solution to return let solution = 0;
 	// 1st check k to see if its zero
-	while (N > 0) {
-		if (N % 2 === 0) {
-			N = N / 2;
-			//increment solution
-			solution += 1;
-			if (K > 0) {
+	while (N > 1) {
+		if (K > 0) {
+			if (N % 2 === 0) {
+				N = N / 2;
+				//increment solution
+				solution += 1;
 				//decrement K
 				K -= 1;
 			}
-		} else {
-			// increment solution
-			solution += 1;
-			//decrement N
-			N -= 1;
 		}
-
-		if (N === 2) {
-			solution += 2;
-			N -= 2;
-		}
-
-		if (N === 1) {
-			solution++;
-			N--;
-		}
-
-		return solution;
+		// increment solution
+		solution += 1;
+		//decrement N
+		N -= 1;
 	}
+	return solution;
 }
+
+console.log(solution(10, 10));
+console.log(solution(8, 18));
