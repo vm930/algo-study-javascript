860acab33431272988255e099f502eca47cf79e6
Victoria Mei <vm930@hotmail.com>
2019-10-06 22:23:45 -0400

    finish tree

--- Maximum Depth of Binary Tree.js
+++ Maximum Depth of Binary Tree.js
@@ -14,6 +14,24 @@ Given binary tree [3,9,20,null,null,15,7],
   9  20
     /  \
    15   7
-   
+
 return its depth = 3.
 */
+
+var maxDepth = function(root) {
+	if (root === null) {
+		return 0;
+	}
+
+	let l_depth = 0;
+	let r_depth = 0;
+
+	if (root.left) {
+		l_depth = maxDepth(root.left);
+	}
+	if (root.right) {
+		r_depth = maxDepth(root.right);
+	}
+
+	return Math.max(l_depth, r_depth) + 1;
+};
