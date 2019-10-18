739bcc6d0f49634514829b363ceb2f76df248f9a
Victoria Mei <vm930@hotmail.com>
2019-10-17 21:11:51 -0400

    next permutation

--- 31.next permutation.js
+++ 31.next permutation.js
@@ -13,8 +13,42 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
 */
-var nextPermutation = function(nums) {};
+var nextPermutation = function(nums) {
+	let breakPoint;
 
-console.log(nextPermutation(1, 2, 3));
-console.log(nextPermutation(3, 2, 1));
-console.log(nextPermutation(1, 1, 5));
+	for (let i = nums.length; i > 0; i--) {
+		console.log(nums);
+		if (nums[i] > nums[i - 1]) {
+			//mark the number
+			breakPoint = nums[i - 1];
+			for (let j = breakPoint + 1; j < nums.length; j++) {
+				//go forward and look for the number that is less than breakpoint
+				if (nums[breakPoint] > nums[j]) {
+					//swap the elements
+					let temp = nums[breakPoint];
+					nums[breakPoint] = nums[j - 1];
+					nums[j - 1] = temp;
+				}
+			}
+
+			let k = breakPoint + 1;
+			let l = nums.length;
+
+			while (k < l) {
+				if (nums[k] > nums[l]) {
+					//swap
+					let temp = nums[k];
+					nums[k] = nums[j];
+					nums[j] = temp;
+				}
+				k++;
+				l--;
+			}
+		}
+	}
+};
+
+console.log(nextPermutation([ 1, 2, 3 ]));
+console.log(nextPermutation([ 3, 2, 1 ]));
+console.log(nextPermutation([ 1, 1, 5 ]));
+console.log(nextPermutation([ 1, 5, 8, 4, 7, 6, 5, 3, 1 ]));
