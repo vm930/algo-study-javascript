21794d1ca4d70a450f2746091b6d55a269a6a8e5
Victoria Mei <vm930@hotmail.com>
2019-12-01 08:25:30 -0500

    get started with the problem

---
+++ midpoint.js
@@ -0,0 +1,13 @@
+// Directions
+//Return the "middle" node of a linked list
+// if the list has an even number of elements, return the node at the end of the frist half of the list.
+//DO NOT  use a counter variable or retrieve the size of the list, and only iterate thru the list one time.
+
+//eg:
+/*
+const l = new LinkedList();
+l.insertLast("a")
+l.insertLast("b")
+l.insertLast("c")
+midpoint(l); => returns {data: "b"}
+*/
