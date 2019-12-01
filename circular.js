ce29e30cf24f24c1b47850ce4ceb2deeb4c5e8fc
Victoria Mei <vm930@hotmail.com>
2019-12-01 09:26:38 -0500

    added another algo problem

---
+++ circular.js
@@ -0,0 +1,16 @@
+/*
+Directions:
+Given a linked list, return true if the list is circular, false if it is not.
+eg:
+
+const l = new List();
+const a = new Node("a")
+const b = new Node("b")
+const c = new Node("c")
+
+*/
+
+function circular(list) {
+	let slow = list.head;
+	let fast = list.head;
+}
